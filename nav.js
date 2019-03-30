11b273c515d8cfe0f955fa93ece3f9b2b79aadc6
anglekto <47034952+anglekto@users.noreply.github.com>
2019-03-29 23:12:45 -0400

    Add files via upload

--- nav.js
+++ nav.js
@@ -1,4 +1,13 @@
 function myFunction() {
+  var x = document.getElementById("mobile");
+  if (x.innerHTML === "X") {
+    x.innerHTML = "MENU";
+  } else {
+    x.innerHTML = "X";
+  }
+}
+
+function myFunction2() {
   var x = document.getElementById("navi");
   if (x.className === "nav-links") {
     x.className += " responsive";

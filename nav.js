e2f83300075680db1139d09e871d34c2d6a56b32
anglekto <47034952+anglekto@users.noreply.github.com>
2019-05-12 22:21:47 -0400

    Add files via upload
    Added the dropdown for Portfolio in nav

--- nav.js
+++ nav.js
@@ -1,9 +1,9 @@
 function myFunction() {
   var x = document.getElementById("mobile");
-  if (x.className === "fas fa-times") {
-    x.className = "mobile-nav";
+  if (x.className === "mobile-nav") {
+    x.className += " responsive";
   } else {
-    x.className = "fas fa-times";
+    x.className = "mobile-nav";
   }
 }
 
@@ -15,3 +15,31 @@ function myFunction2() {
     x.className = "nav-links";
   }
 }
+
+function show() {
+  const z = document.querySelector(".port-drop");
+  z.classList.toggle("show");
+  if (z.style.display === "none" || z.style.display === "") {
+    z.style.display = "block";
+  } else {
+    z.style.display = "none";
+  }
+}
+
+function myFunction3() {
+  var y = document.getElementById("port");
+  if (y.className === "port") {
+    y.className += " responsive";
+  } else {
+    y.className = "port";
+  }
+}
+
+// window.onscroll = function() {
+//   const y = document.getElementById("nav-bar");
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     y.style.backgroundColor = "hsla(0,0%,100%,.9)";
+//   } else {
+//     y.style.backgroundColor = "transparent";
+//   }
+// }
